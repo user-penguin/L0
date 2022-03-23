e3230f4a29c08c163007b836355efd775313db0d
user-penguin <kobzev96@gmail.com>
2022-03-24 06:53:37 +0700

    connect with psql, insert received orders

--- go.mod
+++ go.mod
@@ -3,10 +3,27 @@ module L0
 go 1.17
 
 require (
+	github.com/jackc/pgx/v4 v4.15.0
+	github.com/nats-io/stan.go v0.10.2
+)
+
+require (
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/nats-io/nats.go v1.13.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.11.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.10.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+	github.com/nats-io/nats-server/v2 v2.7.4 // indirect
+	github.com/nats-io/nats-streaming-server v0.24.3 // indirect
+	github.com/nats-io/nats.go v1.13.1-0.20220308171302-2f2f6968e98d // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
-	github.com/nats-io/stan.go v0.10.2 // indirect
-	golang.org/x/crypto v0.0.0-20210314154223-e6e6c4f2bb5b // indirect
+	golang.org/x/crypto v0.0.0-20220307211146-efcb8507fb70 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"L0/model"
+	"context"
 	"encoding/json"
-	"fmt"
+	"github.com/jackc/pgx/v4"
 	"github.com/nats-io/stan.go"
 	"log"
 	"sync"
 )
 
+const psqlUrl = "postgresql://localhost/L0?user=intern&password=.hbqufufhby"
+
 func main() {
+	// устанавливаем соединение с nats-streaming-server
 	sc, err := stan.Connect("test-cluster", "reader")
 	if err != nil {
 		log.Printf("Connections error, reason:%s", err)
@@ -18,20 +22,47 @@ func main() {
 	defer func(sc stan.Conn) {
 		err := sc.Close()
 		if err != nil {
-			log.Println(err)
+			log.Printf("Error closing stan connection. %s", err)
 		}
 	}(sc)
 
-	sc.Subscribe("wild", func(msg *stan.Msg) {
+	// устанавливаем соединение с постгресом
+	conn, err := pgx.Connect(context.Background(), psqlUrl)
+	if err != nil {
+		log.Printf("Error with psql connection. %s", err)
+	}
+	defer func(conn *pgx.Conn, ctx context.Context) {
+		err := conn.Close(ctx)
+		if err != nil {
+			log.Printf("Error closing psql connection. %s", err)
+		}
+	}(conn, context.Background())
+
+	// основное тело, где происходит принятие сообщений из nats-streaming
+	_, err = sc.Subscribe("wild", func(msg *stan.Msg) {
 		var order = &model.Order{}
 		err := json.Unmarshal(msg.Data, order)
 		if err != nil {
-			log.Println(err)
+			log.Printf("Error in NATS-Order unmarhaling. %s", err)
 		}
-		fmt.Printf("order_uid:%s, delivery.name: %s\n", order.OrderUid, order.Delivery.Name)
+		_, _ = insertOrder(*order, *conn)
 	})
+	if err != nil {
+		log.Println(err)
+	}
 
 	w := sync.WaitGroup{}
 	w.Add(1)
 	w.Wait()
+}
+
+func insertOrder(ord model.Order, conn pgx.Conn) (uint64, error) {
+	orderId, _ := ord.Insert(conn)
+	_, _ = ord.Delivery.Insert(orderId, conn)
+	_, _ = ord.Payment.Insert(orderId, conn)
+	for _, item := range ord.Items {
+		_, _ = item.Insert(orderId, conn)
+	}
+	log.Printf("Last inserted order id: %d", orderId)
+	return orderId, nil
 }
